17e902d38515cfb6d59fc619552a3a8b0863ca9f
Hauke Stieler <mail@hauke-stieler.de>
2017-11-28 16:32:08 +0100

    Rename filelen parameter

--- src/cudo.c
+++ src/cudo.c
@@ -14,18 +14,18 @@ void debug(const char *format, ...)
 }
 
 //TODO change names like filelen to file_len or something similar
-void read_from_file(char* file_name, char** buffer, long* filelen)
+void read_from_file(char* file_name, char** buffer, long* file_len)
 {
-	FILE *fileptr;
+	FILE *file_ptr;
 
-	fileptr = fopen(file_name, "rb"); 	// Open the file in binary mode
-	fseek(fileptr, 0, SEEK_END);		// Jump to the end of the file
-	*filelen = ftell(fileptr);			// Get the current byte offset in file
-	rewind(fileptr);					// Jump back to the beginning
+	file_ptr = fopen(file_name, "rb"); 	// Open the file in binary mode
+	fseek(file_ptr, 0, SEEK_END);		// Jump to the end of the file
+	*file_len = ftell(file_ptr);			// Get the current byte offset in file
+	rewind(file_ptr);					// Jump back to the beginning
 
-	*buffer = (char *)calloc(*filelen + 1, sizeof(char)); // file length + \0
-	fread(*buffer, sizeof(char), *filelen, fileptr); // Read in the entire file
-	fclose(fileptr); // Close the file
+	*buffer = (char *)calloc(*file_len + 1, sizeof(char)); // file length + \0
+	fread(*buffer, sizeof(char), *file_len, file_ptr); // Read in the entire file
+	fclose(file_ptr); // Close the file
 }
 
 void write_to_file(char *file_name, char *buffer, long file_len)
